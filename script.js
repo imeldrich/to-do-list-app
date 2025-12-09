854d0faed5b151954db1127b52d11070a4c26ee2
Eldrich Salonga <eldrich.salonga@gmail.com>
2025-12-09 22:44:49 +0800

    add styling and functions

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const add = document.getElementById("add-btn");
 const input = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const checkbox = document.querySelectorAll("input[type='checkbox']");
 const taskFromLocalStorage = JSON.parse(localStorage.getItem("task"));
 
 let tasks = [];
+let index = ""
 
 // add task to the list
 add.addEventListener("click", function() {
+    if (input.value) {
     tasks.push(input.value);
     localStorage.setItem("task", JSON.stringify(tasks));
+    input.value = "";
     renderTasks();
+    }
 });
 
 // render tasks to the page
 function renderTasks() {
     let taskLog = "";
     for (let i = 0; i < tasks.length; i++) {
-        taskLog += `<li>${tasks[i]}</li>`;
-    }
+    index = tasks[i];
+    taskLog += `
+    <li>
+        <input type="checkbox" id="${tasks[i]}" class="checkbox"/>
+        <label for="${tasks[i]}">${tasks[i]}</label>
+    </li>`;
+}
     ulEl.innerHTML = taskLog;
 }
 
@@ -26,3 +36,9 @@ if (taskFromLocalStorage) {
     tasks = taskFromLocalStorage;
     renderTasks();
 }
+
+// if (index) {
+//         tasks.splice(index, 1);
+//         localStorage.setItem("task", JSON.stringify(tasks));
+//         renderTasks();
+//     }
